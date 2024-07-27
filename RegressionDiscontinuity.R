cf8279cfb4021dfb613fa8867d437a0b55b2c342
Seamus-L <you@example.com>
2024-07-27 14:52:38 -0700

    Added skeleton of RD analysis

---
+++ RegressionDiscontinuity.R
@@ -0,0 +1,4 @@
+#This file produces a regression discontinuity estimate 
+#Can conveniently use table-12 files provided by statistics  canada to construct the data set
+
+data <- read.csv("RDData/table_tableau12_42.csv")
